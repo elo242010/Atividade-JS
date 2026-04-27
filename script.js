161818144fdee0075f10c4b3e9ae28c1416abcc9
elo242010 <eloise.s@ba.estudante.senai.br>
2026-04-27 16:19:12 -0300

    Add function to generate multiplication table

---
+++ script.js
@@ -0,0 +1,10 @@
+function gerarTabuada() {
+    let numero = Number(document.getElementById("numero").value);
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = "";
+
+    for (let i = 0; i <= 10; i++) {
+        resultado.innerHTML += numero + " x " + i + " = " + (numero * i) + "<br>";
+    }
+}
