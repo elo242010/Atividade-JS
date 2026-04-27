714fb3de7402ff777f1dce13112afec4db83ebe7
elo242010 <eloise.s@ba.estudante.senai.br>
2026-04-27 17:00:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function gerarTabuada() {
-    let numero = Number(document.getElementById("numero").value);
-    let resultado = document.getElementById("resultado");
 
-    resultado.innerHTML = "";
-
-    for (let i = 0; i <= 10; i++) {
-        resultado.innerHTML += numero + " x " + i + " = " + (numero * i) + "<br>";
-    }
-}
